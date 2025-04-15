2bb09e9ff9b000c8ba43c273ef5adc4956060908
Thu <th858940@dal.ca>
2025-04-15 18:31:53 -0300

    Modify function for finding maximum value in dataframe.

--- R/EVA_Halifax.R
+++ R/EVA_Halifax.R
@@ -1,38 +1,39 @@
-## EVA wind Yarmouth ##
-# Calculating the maximum storm surges for Yarmouth:
-# Using full data
-rm(list=ls())
-full.data = read.csv("Data/linear_interpolation_df.csv") #This part need to be switched to the full dataset created by Thu
+# Calculating the maximum storm surges for Halifax:
+
+full.data = read.csv("Data/linear_interpolation_df.csv")
 
 # Extract two columns by name
-full.max.data = full.data[, c("date", "location","avg_wind_speed")]
-full.max.data.yarmouth = full.max.data[full.max.data$location == "Yarmouth", ]
+full.max.data = full.data[, c("date", "location","avg_water_surge")]
+full.max.data.halifax = full.max.data[full.max.data$location == "Halifax", ]
 
+# library(data.table)
+#
+# # Convert to data.table
+# setDT(full.max.data.halifax)
+#
+# # Extract the annual maximum
+# annual.maximas.hx = full.max.data.halifax[, .(max_storm_surge = max(avg_water_surge, na.rm = TRUE)),
+#                                           by = .(year = year(date))]
+
+# Extract the annual maximum
 library(dplyr)
 library(lubridate)
 
-# library(data.table)
-# # Convert to data.table
-# setDT(full.max.data.yarmouth)
-#
-# # Extract the annual maximum
-# annual.maximas.yarmouth = full.max.data.yarmouth[, .(max_wind = max(avg_wind_speed, na.rm = TRUE)),
-#                                                  by = .(year = year(date))]
-
-annual.maximas.yarmouth <- full.max.data.yarmouth %>%
+annual.maximas.hx <- full.max.data.halifax %>%
   mutate(year = year(date)) %>%
   group_by(year) %>%
-  summarise(max_wind = max(avg_wind_speed, na.rm = TRUE), .groups = "drop")
+  summarise(max_storm_surge = max(avg_water_surge, na.rm = TRUE), .groups = "drop")
 
 # 28 years
-plot(annual.maximas.yarmouth, xlab = "years", ylab= "Storm surge annual maxima")
-title("Yarmouth annual maxima storm surge (1956-2024)")
+plot(annual.maximas.hx, xlab = "Years", ylab = "Storm Surge Annual Maxima", main = "Halifax Annual Maxima Storm Surge (1961-2014)")
 
-# Fit these yarmouth annual maximas using the GEV.
+
+# Fit these Halifax annual maximas using the GEV.
+#| fig.height = 9
 library(ismev)
 library(evd)
 
-gev.surge = gev.fit(annual.maximas.yarmouth$max_wind, )
+gev.surge = gev.fit(annual.maximas.hx$max_storm_surge)
 gev.diag(gev.surge)
 
 # Extract the GEV parameters from the fitted model
@@ -45,11 +46,9 @@ V = gev.surge$cov
 T_values = c(2, 5, 10, 20, 50, 100)
 p = 1/T_values
 yp = -log(1 - p)
-
 # Calculate return levels for each return period
 return_levels = sapply(T_values, function(T) {
   return_level = qgev(1/T, loc = mu, scale = sigma ,shape = xi, lower.tail = FALSE)
-  return_level
 })
 
 grad = matrix(0,nrow = length(p), ncol = 3)
@@ -77,37 +76,35 @@ upper.bounds
 # Plot the return levels for the specified return periods
 plot(T_values, return_levels, type = "b", col = "blue", pch = 19,
      xlab = "Return Period (Years)", ylab = "Return Level",
-     main = "Yarmouth Return Levels for Different Return Periods",ylim = c(min(lower.bounds), max(upper.bounds)))
+     main = "Halifax Return Levels for Different Return Periods", ylim = c(0.45, 0.8))
 
 lines(T_values,lower.bounds, type = "o", pch = 19, col = "red",lty = 2)
 lines(T_values,upper.bounds, type = "o", pch = 19, col = "red",lty = 2)
 
-###############################################################################
-# Same analysis but with an other data set less complete
-# Calculating the maximum storm surges for yarmouth:
-# rm(list=ls())
-# full.data = read.csv("Data/combination_df.csv") #This part need to be switched to the
+# # Calculating the maximum storm surges for Halifax using less data which matches the full timeseries with other variables:
+# all.data = read.csv("Data/combination_df_v1.csv") #This part need to be switched to the full dataset created by Thu
 #
 # # Extract two columns by name
-# full.max.data = full.data[, c("date", "location","avg_wind_speed")]
-# full.max.data.yarmouth = full.max.data[full.max.data$location == "Yarmouth", ]
+# max.data = all.data[, c("date", "location","avg_water_surge")]
+# max.data.halifax = max.data[max.data$location == "Halifax", ]
 #
 # library(data.table)
 #
 # # Convert to data.table
-# setDT(full.max.data.yarmouth)
+# setDT(max.data.halifax)
 #
 # # Extract the annual maximum
-# annual.maximas.yarmouth = full.max.data.yarmouth[, .(max_wind = max(avg_wind_speed, na.rm = TRUE)),
-#                                                  by = .(year = year(date))]
+# annual.maximas.hx = max.data.halifax[, .(max_storm_surge = max(avg_water_surge, na.rm = TRUE)),
+#                                      by = .(year = year(date))]
 # #28 years
-# plot(annual.maximas.yarmouth)
+# plot(annual.maximas.hx)
 #
-# # Fit these yarmouth annual maximas using the GEV.
+# #Fit these Halifax annual maximas using the GEV.
+# #| fig.height = 9
 # library(ismev)
 # library(evd)
 #
-# gev.surge = gev.fit(annual.maximas.yarmouth$max_wind, )
+# gev.surge = gev.fit(annual.maximas.hx$max_storm_surge)
 # gev.diag(gev.surge)
 #
 # # Extract the GEV parameters from the fitted model
@@ -135,7 +132,7 @@ lines(T_values,upper.bounds, type = "o", pch = 19, col = "red",lty = 2)
 #   var_return_levels[i] = t(grad[i,]) %*% V %*% grad[i,]
 # }
 #
-# # Compute Standard Errors of Return Levels
+# # Compute Standard Errors of Return Level
 # se_return_levels =sqrt(var_return_levels)
 #
 # # 95% Confidence Interval
@@ -151,7 +148,8 @@ lines(T_values,upper.bounds, type = "o", pch = 19, col = "red",lty = 2)
 # # Plot the return levels for the specified return periods
 # plot(T_values, return_levels, type = "b", col = "blue", pch = 19,
 #      xlab = "Return Period (Years)", ylab = "Return Level",
-#      main = "Yarmouth Return Levels for Different Return Periods",ylim = c(min(lower.bounds), max(upper.bounds)))
+#      main = "Halifax Return Levels for Different Return Periods", ylim = c(0.4, 1.2))
 #
 # lines(T_values,lower.bounds, type = "o", pch = 19, col = "red",lty = 2)
 # lines(T_values,upper.bounds, type = "o", pch = 19, col = "red",lty = 2)
+
